9f4fde0687b272c1dcc2a2c5141a6dd8e338ca2b
宇須俊也 <>
2023-06-09 23:24:51 +0900

    消費期限の投稿機能を追加

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,14 @@
 class Food < ApplicationRecord
+  has_one_attached :food_img
   belongs_to :user
+
+  enum management: {賞味期限: 1, 消費期限: 2, 管理しない: 3}
+
+  def food_image
+    if food_img.attached?
+      food_img
+    else
+      'no_food.png'
+    end
+  end
 end
